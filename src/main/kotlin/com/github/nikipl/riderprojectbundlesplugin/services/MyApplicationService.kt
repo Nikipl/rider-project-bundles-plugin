3fd073f8be7c9323a074cb3284e0ba698d958576
GitHub Action <action@github.com>
2021-08-03 09:26:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nikipl/riderprojectbundlesplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nikipl.riderprojectbundlesplugin.services
+
+import com.github.nikipl.riderprojectbundlesplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/nikipl/riderprojectbundlesplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nikipl/riderprojectbundlesplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nikipl.riderprojectbundlesplugin.services
 
+import com.github.nikipl.riderprojectbundlesplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
